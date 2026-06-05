9d9bce7ff3ba8ac1df36b25915c709aa628d8fc9
2174064279@qq.com <2174064279@qq.com>
2026-06-05 20:26:37 +0800

    0

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/company-website/', // 替换为你的仓库名
+  base: '/English-learn-company-website/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
